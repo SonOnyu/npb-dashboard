565f79de884d09889f0b68a8ec9002eec8ebbc4e
SonOnyu <153486901+SonOnyu@users.noreply.github.com>
2026-06-15 17:53:38 +0900

    Update npb-analyze.js

--- netlify/functions/npb-analyze.js
+++ netlify/functions/npb-analyze.js
@@ -26,6 +26,7 @@ function callClaude(prompt) {
       headers: {
         'Content-Type': 'application/json',
         'anthropic-version': '2023-06-01',
+        'x-api-key': process.env.ANTHROPIC_API_KEY,
         'Content-Length': Buffer.byteLength(body),
       },
     }, (res) => {
